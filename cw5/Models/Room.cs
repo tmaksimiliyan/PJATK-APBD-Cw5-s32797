f1a74b2d1b08bf67ca0cf0831a2986ab6601eb37
maksymilian <maximustar2006@gmail.com>
2026-04-14 18:46:31 +0200

    Add room and reservation domain models

---
+++ cw5/Models/Room.cs
@@ -0,0 +1,19 @@
+namespace cw5.Models;
+
+public class Room
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; } =string.Empty;
+    
+    public string BuildingCode { get; set; } = string.Empty;
+    
+    public int Floor { get; set; }
+    
+    public int Capacity { get; set; }
+    
+    public bool HasProjector { get; set; }
+    
+    public bool IsActive { get; set; }
+    
+}
